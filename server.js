37a882e2c55655e66f1a440233f42a1eb64e1e49
Dylan Pugh <pughdylan93@gmail.com>
2019-02-25 09:35:17 -0500

    Finished

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 var express = require("express");
-
+var bodyParser = require("body-parser");
+var path = require("path");
 
 var app = express();
 var PORT = process.env.PORT || 8080;
 
 
-var db = require("./data/friends.js");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json({type: "application/*+json"}));
+
+app.use(bodyParser.raw({type: "application/vnd.custom-type"}));
+
+app.use(bodyParser.text({type: "text/html"}));
+
+require("./app/routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
 
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
-app.use(express.static("public"));
-
-
-require("./routes/apiRoutes.js")(app);
-
-
-db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
+app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
-  });
 });
